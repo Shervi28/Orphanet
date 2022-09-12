11679415b01edaa25b59f87416b025fe3267aa72
HurricaneProdigy <shervinantony28@gmail.com>
2022-09-11 18:06:24 -0700

    Connect Feature Done
    Built the sorting algorithm(not very efficient O(n) time complexity but this is just a prototype). I revamped the saving data mechanism by using a .csv file database instead of a .txt.

--- connect.py
+++ connect.py
@@ -1,29 +1,47 @@
 import streamlit as st
+import csv
 
 st.write("Enter your Name, Hobbies and Interests")
 
 name = st.text_input("Enter your Name")
 interests = st.text_input("Enter 1 of your biggest interests")
 hobbies = st.text_input("Enter 1 of your biggest hobbies")
-
-def save_input(x,y,z):
-    with open('connect_data.txt', 'a') as f:
-        f.write(x)
-        f.write(" ")
-        f.write(y)
-        f.write(" ")
-        f.write(z)
-        f.write('\n')
-
 submit = st.button("Submit")
 
+st.write("Find Friends Here!")
+interest = st.text_input("Type your interests to search up people with similar interests as you!")
+hobby = st.text_input("Type your hobbies to search up people with similar hobbies as you!")
+search = st.button("Search")
+
+headersCSV = ['Name','Interests','Hobbies'] 
+
+def save_input():
+    dict = {"Name": name, "Interests": interests, "Hobbies": hobbies}
+
+    with open('data.csv','a', newline='') as csv_file:
+        dict_object = csv.DictWriter(csv_file, fieldnames=headersCSV)
+        dict_object.writerow(dict)
+
+
+
 if submit:
-    save_input(name, interests, hobbies)
+    save_input()
 
-with open('connect_data.txt','r') as file:
-    for line in file:
-        Line = line.strip()
-        print(Line)
+if search:
+    with open('data.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print("Data")
+                line_count +=1
+            else:
+                if interest  == row[1] :
+                    st.write(f" We have found {row[0]} as a potential friend based on your interests!")
+                elif hobby == row[2]:
+                    st.write(f" We have found {row[0]} as a potential friend based on your interests!")
+                print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+                line_count += 1
+                
+        print(f"processed {line_count} lines.")
 
-        for word in Line:
-            pass
